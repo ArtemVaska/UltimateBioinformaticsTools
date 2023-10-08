4a6a596e1ba7859bd8860d588c27c51ced551ffa
Artem Vasilev <artem_vasilev_01@list.ru>
2023-10-08 08:53:14 +0300

    Beautify ultimate_tools.py

--- ultimate_tools.py
+++ ultimate_tools.py
@@ -1,8 +1,8 @@
 from typing import List, Tuple, Dict, Union
 
-import src.fastq_tools as fastq_tools
-import src.dna_rna_tools as dna_rna_tools
-import src.protein_tools as protein_tools
+from src import fastq_tools
+from src import dna_rna_tools
+from src import protein_tools
 
 
 def filter_fastq_seqs(
@@ -16,28 +16,32 @@ def filter_fastq_seqs(
 
     Args:
     - seqs (Dict[str, Tuple[str]]): a dictionary with FASTQ-file contents
-    - gc_bounds (Union[float, Tuple[float]]): GC-content boundaries (from 0 to 100)
+    - gc_bounds (Union[float, Tuple[float]]):
+        GC-content boundaries (from 0 to 100)
         within which filtered sequences must be included
     - length_bounds (Union[int, Tuple[int]]): length boundaries
         within which filtered sequences must be included
-    - quality_threshold (int): reading quality value according to table phred+33,
+    - quality_threshold (int): reading quality value
+        according to table phred+33,
         below which filtering will not be performed
 
     Return:
     - Dict[str, Tuple[str]]: a dictionary with filtered sequences
     """
 
-    if isinstance(gc_bounds, (int, float)):  # проверка инпута и указание границ
+    if isinstance(gc_bounds, (int, float)):  # input check
         gc_bounds = (0, gc_bounds)
     if isinstance(length_bounds, int):
         length_bounds = (0, length_bounds)
     if gc_bounds[1] > 100 or \
             (gc_bounds[0] > gc_bounds[1]) or \
             (length_bounds[0] > length_bounds[1]) or \
-            not (0 <= quality_threshold <= 40):  # проверка указания границ
+            not 0 <= quality_threshold <= 40:  # bounds check
         raise ValueError('The bounds are indicated incorrectly!')
-    seqs_gc_and_len_in_bounds = fastq_tools.is_gc_and_length_in_bounds(seqs, gc_bounds, length_bounds)
-    quality_in_bounds = fastq_tools.is_quality_in_bounds(seqs, quality_threshold)
+    seqs_gc_and_len_in_bounds = (
+        fastq_tools.is_gc_and_length_in_bounds(seqs, gc_bounds, length_bounds))
+    quality_in_bounds = (
+        fastq_tools.is_quality_in_bounds(seqs, quality_threshold))
 
     filtered_fastq_seqs = {}
     for key in seqs:
@@ -53,7 +57,8 @@ def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
     Accepts sequences and action to be done with them.
 
     Args:
-    The last argument is the action performed on the sequences that are the other arguments
+    The last argument is the action performed on the sequences
+        that are the other arguments
 
     Possible actions:
     - transcribe
@@ -75,7 +80,8 @@ def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
     result_list = []
     show_warning_message = False
     for seq in seqs:
-        if (set(seq) <= set('AUTGCautgc')) is False or 'T' in seq.upper() and 'U' in seq.upper():
+        if ((set(seq) <= set('AUTGCautgc')) is False or
+                'T' in seq.upper() and 'U' in seq.upper()):
             result_list.append(None)
             show_warning_message = True
         else:
@@ -87,8 +93,7 @@ def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
 
     if len(result_list) == 1:
         return result_list[0]
-    else:
-        return result_list
+    return result_list
 
 
 def run_ultimate_protein_tools(*args: Union[str, List[str]], **kwargs) -> list:
@@ -96,7 +101,8 @@ def run_ultimate_protein_tools(*args: Union[str, List[str]], **kwargs) -> list:
     Accepts command and runs it on input data with parameters.
 
     Args:
-    - args (Union[str, List[str]]): the first argument is a command to do with sequences, which are other arguments
+    - args (Union[str, List[str]]): the first argument is a command to do
+        with sequences, which are other arguments
     - kwargs (str): various arguments for specific command
 
     Possible commands:
@@ -139,6 +145,4 @@ def run_ultimate_protein_tools(*args: Union[str, List[str]], **kwargs) -> list:
         print('Some of your sequences have mistakes!')
     if len(result_list) == 1:
         return result_list[0]
-    else:
-        return result_list
-    
+    return result_list
