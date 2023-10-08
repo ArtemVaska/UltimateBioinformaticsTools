9932d8d8cf666fa79b98adbd1467aa17a53c5993
Artem Vasilev <artem_vasilev_01@list.ru>
2023-10-08 06:18:58 +0300

    Rename dna_tools.py to dna_rna_tools.py and add constants

---
+++ src/dna_rna_tools.py
@@ -0,0 +1,12 @@
+ALPHABET_FOR_TRANSCRIBE = {
+    'T': 'U', 't': 'u',
+    'U': 'T', 'u': 't'
+    }
+
+ALPHABET_FOR_COMPLEMENT = {
+    'A': 'T', 'a': 't',
+    'G': 'C', 'g': 'c',
+    'T': 'A', 't': 'a',
+    'C': 'G', 'c': 'g',
+    'U': 'A', 'u': 'a'
+    }
