765382c34206a5ee14a0ed29d25e6b9b42fe6b52
ArtemVaska <artem_vasilev_01@list.ru>
2024-05-17 21:30:50 +0300

    Update convert_multiline_fasta_to_oneline function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,41 +1,6 @@
 from dataclasses import dataclass
 
 
-def convert_multiline_fasta_to_oneline(
-    input_fasta: str, output_fasta: str = None
-) -> dict:
-    """
-    Creates oneline fasta from multiline fasta.
-    :param input_fasta: input filename
-    :param output_fasta: output filename; <input_filename>_oneline.fasta if not provided
-    :return: a dictionary with id of sequence and its sequence
-    """
-
-    seqs = {}
-    with open(input_fasta) as multiline_fasta:
-        seq = []
-        for line in multiline_fasta:
-            line = line.strip()
-            if line.startswith(">"):
-                if len(seq) != 0:
-                    seqs[seq_id] = "".join(seq)
-                    seq = []
-                seq_id = line
-            else:
-                seq.extend(line)
-        seqs[seq_id] = "".join(seq)
-
-    if output_fasta is None:
-        output_fasta = input_fasta.split(".")[0] + "_oneline.fasta"
-
-    with open(output_fasta, mode="w") as oneline_fasta:
-        for seq_id, seq in seqs.items():
-            oneline_fasta.write(f"{seq_id}\n")
-            oneline_fasta.write(f"{seq}\n")
-
-    return seqs
-
-
 @dataclass
 class FastaRecord:
     id_: str
@@ -100,3 +65,21 @@ class OpenFasta:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.handler:
             self.handler.close()
+
+
+def convert_multiline_fasta_to_oneline(
+    input_fasta: str, output_fasta: str = None
+) -> None:
+    """
+    Creates oneline fasta from multiline fasta.
+    :param input_fasta: input filename
+    :param output_fasta: output filename; <input_filename>_oneline.fasta if not provided
+    """
+
+    if output_fasta is None:
+        output_fasta = input_fasta.split(".")[0] + "_oneline.fasta"
+
+    with OpenFasta(input_fasta) as multiline_fasta:
+        with open(output_fasta, "w") as oneline_fasta:
+            for record in multiline_fasta:
+                oneline_fasta.write(f"{record.__repr__()}\n")
