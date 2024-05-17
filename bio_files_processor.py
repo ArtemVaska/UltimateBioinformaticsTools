0692c3c8e0885874e03a9265faf0ff3724ee9b14
ArtemVaska <artem_vasilev_01@list.ru>
2024-05-17 19:51:11 +0300

    Update convert_multiline_fasta_to_oneline function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -2,51 +2,36 @@ from typing import List, Dict
 
 
 def convert_multiline_fasta_to_oneline(
-        input_fasta: str,
-        output_fasta: str = None
-) -> Dict[str, str]:
+    input_fasta: str, output_fasta: str = None
+) -> dict:
     """
     Creates oneline fasta from multiline fasta.
-
-    Args:
-
-    - input_fasta (str): input filename without .fasta extension
-
-    - output_fasta (str): output filename without .fasta extension
-
-    Return:
-
-    - Dict[str, str]: a dictionary with id of sequence and its sequence
-
+    :param input_fasta: input filename
+    :param output_fasta: output filename; <input_filename>_oneline.fasta if not provided
+    :return: a dictionary with id of sequence and its sequence
     """
 
     seqs = {}
-    with open(input_fasta + '.fasta') as multiline_fasta:
+    with open(input_fasta) as multiline_fasta:
         seq = []
         for line in multiline_fasta:
             line = line.strip()
-            if line.startswith('>'):
+            if line.startswith(">"):
                 if len(seq) != 0:
-                    seqs[name] = ''.join(seq)
+                    seqs[seq_id] = "".join(seq)
                     seq = []
-                name = line
+                seq_id = line
             else:
                 seq.extend(line)
-        seqs[name] = ''.join(seq)
+        seqs[seq_id] = "".join(seq)
 
     if output_fasta is None:
-        warning_message = True
-        output_fasta = input_fasta + '_oneline'
-    else:
-        warning_message = False
+        output_fasta = input_fasta.split(".")[0] + "_oneline.fasta"
 
-    with open(output_fasta + '.fasta', mode='w') as oneline_fasta:
-        for name, seq in seqs.items():
-            oneline_fasta.write(name + '\n')
-            oneline_fasta.write(seq + '\n')
-
-    if warning_message:
-        print("Output_fasta wasn't provided.\n<input_fasta>_oneline created!")
+    with open(output_fasta, mode="w") as oneline_fasta:
+        for seq_id, seq in seqs.items():
+            oneline_fasta.write(f"{seq_id}\n")
+            oneline_fasta.write(f"{seq}\n")
 
     return seqs
 
@@ -67,42 +52,44 @@ def read_gbk(input_gbk: str) -> Dict[str, List[str]]:
     """
 
     translations = {}
-    with open(input_gbk + '.gbk') as gbk:
+    with open(input_gbk + ".gbk") as gbk:
         translation = []
         cds_found = False
         translation_found = False
         gene_found = False
         # cds_counter = 0
         for line in gbk:
-            if line.find('CDS') != -1:
+            if line.find("CDS") != -1:
                 cds_found = True
                 # cds_counter += 1
             elif cds_found:
                 if not gene_found:
-                    if line.find('/gene=') != -1:
-                        gene = line[line.find('=')+1:].strip('"\n')
+                    if line.find("/gene=") != -1:
+                        gene = line[line.find("=") + 1 :].strip('"\n')
                     else:
-                        gene = ''
+                        gene = ""
                     gene_found = True
 
-                if line.find('/locus_tag=') != -1:
-                    locus_tag = line[line.find('=')+1:].strip('"\n')
+                if line.find("/locus_tag=") != -1:
+                    locus_tag = line[line.find("=") + 1 :].strip('"\n')
 
-                if line.find('/translation') != -1:
+                if line.find("/translation") != -1:
                     translation_found = True
-                    name = gene if gene != '' else locus_tag
+                    name = gene if gene != "" else locus_tag
                     if line.count('"') == 2:
-                        translations[name] = [line[line.find('=')+1:].strip('"\n')]  # + [cds_counter]
+                        translations[name] = [
+                            line[line.find("=") + 1 :].strip('"\n')
+                        ]  # + [cds_counter]
                         translation = []
                         cds_found = False
                         translation_found = False
                         gene_found = False
                     else:
-                        translation.extend(line[line.find('"')+1:].strip())
+                        translation.extend(line[line.find('"') + 1 :].strip())
                 elif translation_found:
                     if line.find('"') != -1:
-                        translation.extend(line[21:line.find('"')])
-                        translations[name] = [''.join(translation)]  # + [cds_counter]
+                        translation.extend(line[21 : line.find('"')])
+                        translations[name] = ["".join(translation)]  # + [cds_counter]
                         translation = []
                         cds_found = False
                         translation_found = False
@@ -113,11 +100,11 @@ def read_gbk(input_gbk: str) -> Dict[str, List[str]]:
 
 
 def select_genes_from_gbk_to_fasta(
-        input_gbk: str,
-        # genes: List[str],
-        # n_before: int = 1,
-        # n_after: int = 1,
-        output_fasta: str = None
+    input_gbk: str,
+    # genes: List[str],
+    # n_before: int = 1,
+    # n_after: int = 1,
+    output_fasta: str = None,
 ) -> Dict[str, List[str]]:
     """
     Writes found CDS' in .fasta extension.
@@ -141,10 +128,10 @@ def select_genes_from_gbk_to_fasta(
     else:
         warning_message = False
 
-    with open(output_fasta + '.fasta', mode='w') as output_fasta:
+    with open(output_fasta + ".fasta", mode="w") as output_fasta:
         for name, translation in translations.items():
-            output_fasta.write('>' + name + '\n')
-            output_fasta.write(translations[name][0] + '\n\n')
+            output_fasta.write(">" + name + "\n")
+            output_fasta.write(translations[name][0] + "\n\n")
 
     if warning_message:
         print("Output_fasta wasn't provided.\n<input_gbk>.fasta created!")
