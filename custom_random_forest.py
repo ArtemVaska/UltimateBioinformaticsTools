2c25768808dc7cf97a0477fdc54e73798da8a880
ArtemVaska <artem_vasilev_01@list.ru>
2024-05-19 03:10:42 +0300

    Add parallel realization for fit

--- custom_random_forest.py
+++ custom_random_forest.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ProcessPoolExecutor
+
 import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
@@ -7,7 +9,7 @@ SEED = 12345
 
 class RandomForestClassifierCustom(BaseEstimator):
     def __init__(
-            self, n_estimators=10, max_depth=None, max_features=None, random_state=SEED
+        self, n_estimators=10, max_depth=None, max_features=None, random_state=SEED
     ):
         self.n_estimators = n_estimators
         self.max_depth = max_depth
@@ -17,38 +19,53 @@ class RandomForestClassifierCustom(BaseEstimator):
         self.trees = []
         self.feat_ids_by_tree = []
 
-    def fit(self, X, y):
-        self.classes_ = sorted(np.unique(y))
+    def generate_dt(self, args):
+        tree_id, X, y = args
+
         n_samples, n_features = X.shape
+        np.random.seed(self.random_state + tree_id)
 
-        for i in range(self.n_estimators):
-            np.random.seed(self.random_state + i)
+        feature_idx = np.random.choice(
+            n_features, size=self.max_features, replace=False
+        )
 
-            feature_idx = np.random.choice(n_features, size=self.max_features, replace=False)
-            self.feat_ids_by_tree.append(feature_idx)
+        bootstrap_idx = np.random.choice(n_samples, size=n_samples, replace=True)
+        X_bootstrap = X[bootstrap_idx]
+        y_bootstrap = y[bootstrap_idx]
 
-            bootstrap_idx = np.random.choice(n_samples, size=n_samples, replace=True)
-            X_bootstrap = X[bootstrap_idx]
-            y_bootstrap = y[bootstrap_idx]
+        tree = DecisionTreeClassifier(
+            max_depth=self.max_depth,
+            max_features=self.max_features,
+            random_state=self.random_state + tree_id,
+        )
+        tree.fit(X_bootstrap[:, feature_idx], y_bootstrap)
 
-            tree = DecisionTreeClassifier(
-                max_depth=self.max_depth,
-                max_features=self.max_features,
-                random_state=self.random_state + i
+        return tree, feature_idx
+
+    def fit(self, X, y, n_jobs=1):
+        with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+            results = list(
+                executor.map(
+                    self.generate_dt,
+                    [(tree_id, X, y) for tree_id in range(self.n_estimators)],
+                )
             )
-            tree.fit(X_bootstrap[:, self.feat_ids_by_tree[i]], y_bootstrap)
-            self.trees.append(tree)
+
+        self.trees = [results[i][0] for i in range(len(results))]
+        self.feat_ids_by_tree = [results[i][1] for i in range(len(results))]
+
         return self
 
-    def predict_proba(self, X):
+    def predict_proba(self, X, n_jobs=1):
         probas = 0
         for i, tree in enumerate(self.trees):
             probas += tree.predict_proba(X[:, self.feat_ids_by_tree[i]])
         avg_probas = probas / self.n_estimators
+
         return avg_probas
 
-    def predict(self, X):
-        probas = self.predict_proba(X)
+    def predict(self, X, n_jobs=1):
+        probas = self.predict_proba(X, n_jobs)
         predictions = np.argmax(probas, axis=1)
 
         return predictions
