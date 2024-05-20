cb3ae482c818efb572b80c3fbb4bee86a1993829
ArtemVaska <artem_vasilev_01@list.ru>
2024-05-20 11:58:12 +0300

    Update structure with classes

--- test_ubt.py
+++ test_ubt.py
@@ -12,42 +12,6 @@ from ultimate_tools import (
 )
 
 
-def test_genscan_url():
-    target = "http://argonaute.mit.edu/cgi-bin/genscanw_py.cgi"
-    result = URL
-    assert result == target
-
-
-def test_genscan_organisms():
-    target = ["Vertebrate", "Arabidopsis", "Maize"]
-    result = ORGANISMS
-    assert result == target
-
-
-def test_genscan_exon_cutoffs():
-    target = [1.00, 0.50, 0.25, 0.10, 0.05, 0.02, 0.01]
-    result = EXON_CUTOFFS
-    assert result == target
-
-
-def test_dna_alphabet():
-    seq = "ATGCU"
-    with pytest.raises(WrongAlphabetError):
-        DNASequence(seq).is_alphabet_correct()
-
-
-def test_rna_alphabet():
-    seq = "ATGCU"
-    with pytest.raises(WrongAlphabetError):
-        RNASequence(seq).is_alphabet_correct()
-
-
-def test_protein_alphabet():
-    seq = "FLSYZ"
-    with pytest.raises(WrongAlphabetError):
-        AminoAcidSequence(seq).is_alphabet_correct()
-
-
 @pytest.fixture
 def tmp_file():
     file_path = "tmp.fasta"
@@ -56,17 +20,52 @@ def tmp_file():
         os.remove(file_path)
 
 
-def test_oneline_fasta_exists(tmp_file):
-    convert_multiline_fasta_to_oneline("data/example.fasta", tmp_file)
-    assert os.path.exists(tmp_file)
+class TestGenscan:
+    def test_genscan_url(self):
+        target = "http://argonaute.mit.edu/cgi-bin/genscanw_py.cgi"
+        result = URL
+        assert result == target
+
+    def test_genscan_organisms(self):
+        target = ["Vertebrate", "Arabidopsis", "Maize"]
+        result = ORGANISMS
+        assert result == target
+
+    def test_genscan_exon_cutoffs(self):
+        target = [1.00, 0.50, 0.25, 0.10, 0.05, 0.02, 0.01]
+        result = EXON_CUTOFFS
+        assert result == target
 
 
-def test_oneline_fasta_content():
-    with OpenFasta("data/example.fasta") as fasta:
-        target_seqs = fasta.read_records()
+class TestSeqClasses:
+    def test_dna_alphabet(self):
+        seq = "ATGCU"
+        with pytest.raises(WrongAlphabetError):
+            DNASequence(seq).is_alphabet_correct()
 
-    convert_multiline_fasta_to_oneline("data/example.fasta")
-    with OpenFasta("data/example_oneline.fasta") as fasta:
-        result_seqs = fasta.read_records()
-    os.remove("data/example_oneline.fasta")
-    assert target_seqs == result_seqs
+    def test_rna_alphabet(self):
+        seq = "ATGCU"
+        with pytest.raises(WrongAlphabetError):
+            RNASequence(seq).is_alphabet_correct()
+
+    def test_protein_alphabet(self):
+        seq = "FLSYZ"
+        with pytest.raises(WrongAlphabetError):
+            AminoAcidSequence(seq).is_alphabet_correct()
+
+
+class TestFastaFiles:
+
+    def test_oneline_fasta_exists(self, tmp_file):
+        convert_multiline_fasta_to_oneline("data/example.fasta", tmp_file)
+        assert os.path.exists(tmp_file)
+
+    def test_oneline_fasta_content(self):
+        with OpenFasta("data/example.fasta") as fasta:
+            target_seqs = fasta.read_records()
+
+        convert_multiline_fasta_to_oneline("data/example.fasta")
+        with OpenFasta("data/example_oneline.fasta") as fasta:
+            result_seqs = fasta.read_records()
+        os.remove("data/example_oneline.fasta")
+        assert target_seqs == result_seqs
