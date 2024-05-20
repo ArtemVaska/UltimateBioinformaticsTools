484615d8547df24688de69bcd8fab3e53b7e940c
ArtemVaska <artem_vasilev_01@list.ru>
2024-05-20 11:53:59 +0300

    Add tests for files

--- test_ubt.py
+++ test_ubt.py
@@ -1,5 +1,8 @@
+import os
+
 import pytest
 
+from bio_files_processor import convert_multiline_fasta_to_oneline, OpenFasta
 from genscan_scripts import URL, ORGANISMS, EXON_CUTOFFS
 from ultimate_tools import (
     DNASequence,
@@ -43,3 +46,27 @@ def test_protein_alphabet():
     seq = "FLSYZ"
     with pytest.raises(WrongAlphabetError):
         AminoAcidSequence(seq).is_alphabet_correct()
+
+
+@pytest.fixture
+def tmp_file():
+    file_path = "tmp.fasta"
+    yield file_path
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
+def test_oneline_fasta_exists(tmp_file):
+    convert_multiline_fasta_to_oneline("data/example.fasta", tmp_file)
+    assert os.path.exists(tmp_file)
+
+
+def test_oneline_fasta_content():
+    with OpenFasta("data/example.fasta") as fasta:
+        target_seqs = fasta.read_records()
+
+    convert_multiline_fasta_to_oneline("data/example.fasta")
+    with OpenFasta("data/example_oneline.fasta") as fasta:
+        result_seqs = fasta.read_records()
+    os.remove("data/example_oneline.fasta")
+    assert target_seqs == result_seqs
